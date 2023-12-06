5a4408098960fea861924a0eacd3ff8c7005a987
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-12-06 16:28:50 -0300

    Update haiku1.py

--- code/haiku1.py
+++ code/haiku1.py
@@ -1,20 +1,26 @@
 import random
 from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
+import os
+import time
+from termcolor import colored
+from pyfiglet import Figlet
+
+def clear_screen():
+    os.system('clear')  # For Linux
 
 def get_random_haiku():
-    # IDs for haikus or poems in the Project Gutenberg collection
     haiku_ids = [5732, 2049, 768, 53688, 2189]  # Example IDs; add more as needed
-
-    # Choose a random haiku ID
     random_haiku_id = random.choice(haiku_ids)
-
-    # Fetch and clean the text
     haiku_text = strip_headers(load_etext(random_haiku_id)).strip()
-
     return haiku_text
 
-# Rest of the code remains unchanged
+def display_stylized_text(text):
+    color = random.choice(['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'])
+    font_style = random.choice(['block', 'colossal', 'banner', 'graffiti'])
+    figlet = Figlet(font=font_style, justify='center')
+    ascii_art = figlet.renderText(text)
+    print(colored(ascii_art, color))
 
 def main():
     while True:
@@ -24,7 +30,6 @@ def main():
         if random_haiku:
             display_stylized_text(random_haiku)
 
-        time.sleep(5)  # Adjust the sleep duration as needed
+        time.sleep(5)
 
 if __name__ == "__main__":
-    main()
