8e2fa5a5c079cac3c85206518c4c291d098744a6
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-12-08 10:35:55 -0300

    Update pyramid.py

--- code/pyramid.py
+++ code/pyramid.py
@@ -1,13 +1,39 @@
-def generate_pyramid(height):
-    for i in range(1, height + 1):
-        spaces = ' ' * (height - i)
-        triangles = '*' * (2 * i - 1)
-        print(f"{spaces}{triangles}")
+import turtle
+
+def sierpinski_triangle(turtle, order, size):
+    if order == 0:
+        for _ in range(3):
+            turtle.forward(size)
+            turtle.left(120)
+    else:
+        size /= 2
+        sierpinski_triangle(turtle, order - 1, size)
+        turtle.forward(size)
+        sierpinski_triangle(turtle, order - 1, size)
+        turtle.backward(size)
+        turtle.left(60)
+        turtle.forward(size)
+        turtle.right(60)
+        sierpinski_triangle(turtle, order - 1, size)
+        turtle.left(60)
+        turtle.backward(size)
+        turtle.right(60)
 
 def main():
-    pyramid_height = 5  # You can adjust the height of the pyramid
+    window = turtle.Screen()
+    window.bgcolor("white")
 
-    generate_pyramid(pyramid_height)
+    fractal_turtle = turtle.Turtle()
+    fractal_turtle.speed(2)
+    fractal_turtle.penup()
+    fractal_turtle.goto(-150, -150)
+    fractal_turtle.pendown()
+
+    order = 3  # You can adjust the order of the Sierpinski Triangle
+
+    sierpinski_triangle(fractal_turtle, order, 300)
+
+    window.exitonclick()
 
 if __name__ == "__main__":
     main()
