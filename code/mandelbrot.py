8149428f856f702d67cb622046e70c6e08de5bc1
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-12-08 10:20:26 -0300

    Create mandelbrot.py

---
+++ code/mandelbrot.py
@@ -0,0 +1,43 @@
+import os
+import time
+import numpy as np
+
+def clear_screen():
+    os.system('clear')  # For Linux
+
+def mandelbrot(c, max_iter):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < max_iter:
+        z = z*z + c
+        n += 1
+    return n
+
+def display_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter):
+    for row in range(height):
+        for col in range(width):
+            x = np.interp(col, [0, width], [x_min, x_max])
+            y = np.interp(row, [0, height], [y_min, y_max])
+            c = complex(x, y)
+
+            color = mandelbrot(c, max_iter)
+            # Adjust the character based on the color intensity
+            char = ' ' if color == max_iter else '@'
+
+            print(char, end='')
+        print()
+
+def main():
+    width = 80
+    height = 24
+    x_min, x_max = -2, 1
+    y_min, y_max = -1, 1
+    max_iter = 50
+
+    while True:
+        clear_screen()
+        display_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
