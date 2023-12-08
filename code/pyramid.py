f2502dfab1ede49eef31c7a7fccb9a23f7a6f974
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-12-08 15:50:06 -0300

    Update pyramid.py

--- code/pyramid.py
+++ code/pyramid.py
@@ -1,15 +1,15 @@
 def draw_sierpinski_triangle(order, size, x, y):
     if order == 0:
         # Draw a filled triangle
-        for i in range(size):
-            spaces = " " * (size - i - 1)
+        for i in range(int(size)):
+            spaces = " " * (int(size) - i - 1)
             triangles = "*" * (2 * i + 1)
             print(spaces + triangles)
     else:
         # Recursively draw three smaller triangles
-        draw_sierpinski_triangle(order - 1, size / 2, x, y)
-        draw_sierpinski_triangle(order - 1, size / 2, x + size / 2, y)
-        draw_sierpinski_triangle(order - 1, size / 2, x + size / 4, y - size * 0.866 / 2)
+        draw_sierpinski_triangle(order - 1, int(size / 2), x, y)
+        draw_sierpinski_triangle(order - 1, int(size / 2), x + size / 2, y)
+        draw_sierpinski_triangle(order - 1, int(size / 2), x + size / 4, y - size * 0.866 / 2)
 
 def main():
     order = 3  # Adjust the order of the Sierpinski Triangle
