93c6d4e60783cbbaeebd0672536b52c810e6a545
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2023-12-10 12:45:08 -0300

    Update mandelbrot.py

--- code/mandelbrot.py
+++ code/mandelbrot.py
@@ -1,9 +1,13 @@
+import colorama
+import math
+import random
 import os
 import time
-import numpy as np
+
+colorama.init(autoreset=True)
 
 def clear_screen():
-    os.system('clear')  # For Linux
+    os.system('clear' if os.name == 'posix' else 'cls')
 
 def mandelbrot(c, max_iter):
     z = 0
@@ -11,36 +15,46 @@ def mandelbrot(c, max_iter):
     while abs(z) <= 2 and n < max_iter:
         z = z*z + c
         n += 1
+    if n == max_iter:
+        return 0
     return n
 
-def display_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter):
+def generate_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter):
+    pixels = [[' ' for _ in range(width)] for _ in range(height)]
     for row in range(height):
         for col in range(width):
-            x = np.interp(col, [0, width], [x_min, x_max])
-            y = np.interp(row, [0, height], [y_min, y_max])
+            x = x_min + (x_max - x_min) * col / (width - 1)
+            y = y_min + (y_max - y_min) * row / (height - 1)
             c = complex(x, y)
+            val = mandelbrot(c, max_iter)
+            pixels[row][col] = val
+    return pixels
 
-            color = mandelbrot(c, max_iter)
-            # Adjust the character based on the color intensity
-            char = ' ' if color == max_iter else '@'
-
-            print(char, end='')
+def display_mandelbrot(pixels):
+    for row in pixels:
+        for val in row:
+            if val == 0:
+                print(colorama.Back.BLACK + ' ', end='')
+            else:
+                color = random.choice([colorama.Back.RED, colorama.Back.GREEN, colorama.Back.YELLOW, colorama.Back.BLUE])
+                print(color + ' ', end='')
         print()
 
 def main():
-    width = 80
-    height = 24
+    try:
+        width, height = os.get_terminal_size()
+    except:
+        width, height = 80, 24  # Default size if terminal size cannot be determined
+
     x_min, x_max = -2, 1
     y_min, y_max = -1, 1
-    initial_max_iter = 50
+    max_iter = 50
 
-    i = 0
     while True:
-        max_iter = initial_max_iter + i * 10
         clear_screen()
-        display_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter)
-        time.sleep(1)
-        i += 1
+        pixels = generate_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter)
+        display_mandelbrot(pixels)
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     main()
